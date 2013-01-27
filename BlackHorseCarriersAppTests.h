28bc32d348a9aba556e067fc1dfaafc134554ce5
Bob Stevenson <rstevenson555@comcast.net>
2013-01-27 14:15:39 -0600

    adding tests and missing json files

---
+++ BlackHorseCarriersAppTests.h
@@ -0,0 +1,13 @@
+//
+//  BlackHorseCarriersAppTests.h
+//  BlackHorseCarriersAppTests
+//
+//  Created by Robert Stevenson on 8/12/12.
+//  Copyright (c) 2012 Robert Stevenson. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BlackHorseCarriersAppTests : SenTestCase
+
+@end
---
+++ BlackHorseCarriersAppTests.m
@@ -0,0 +1,32 @@
+//
+//  BlackHorseCarriersAppTests.m
+//  BlackHorseCarriersAppTests
+//
+//  Created by Robert Stevenson on 8/12/12.
+//  Copyright (c) 2012 Robert Stevenson. All rights reserved.
+//
+
+#import "BlackHorseCarriersAppTests.h"
+
+@implementation BlackHorseCarriersAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BlackHorseCarriersAppTests");
+}
+
+@end
