0397b0c8eb78dfd06b752008804e676b75e779cf
Bob Stevenson <rstevenson555@comcast.net>
2013-01-28 21:04:00 -0600

    use #defines for constants

--- BlackHorseCarriersApp/Constants.h
+++ BlackHorseCarriersApp/Constants.h
@@ -9,10 +9,10 @@
 #ifndef BlackHorseCarriersApp_Constants_h
 #define BlackHorseCarriersApp_Constants_h
 
-static NSString* JOB_APPLICATION_URL =@"https://www.blackhorsecarriers.com/application.php";
-static NSString* IPAY_ADP_URL = @"https://ipay.adp.com";
-//static NSString* CONTACT_US_URL = @"http://www.blackhorsecarriers.com/submitcontactus_test.php";
-static NSString* CONTACT_US_URL = @"http://www.blackhorsecarriers.com/submitcontactus.php";
-static NSString * GET_TERMINALS_URL = @"http://www.blackhorsecarriers.com/phpsqlsearch_genxml.php?lat=37&lng=-122&radius=10025&initial=1";
+#define JOB_APPLICATION_URL @"https://www.blackhorsecarriers.com/application.php"
+#define IPAY_ADP_URL @"https://ipay.adp.com"
+//static NSString* CONTACT_US_URL = @"http://www.blackhorsecarriers.com/submitcontactus_test.php"
+#define CONTACT_US_URL @"http://www.blackhorsecarriers.com/submitcontactus.php"
+#define GET_TERMINALS_URL @"http://www.blackhorsecarriers.com/phpsqlsearch_genxml.php?lat=37&lng=-122&radius=10025&initial=1"
 
 #endif
