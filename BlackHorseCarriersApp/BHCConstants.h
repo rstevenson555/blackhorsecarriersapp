d9082e81940126ebd781531c88897674883e570e
Bob Stevenson <rstevenson555@comcast.net>
2013-03-19 06:16:40 -0500

    new constants files

---
+++ BlackHorseCarriersApp/BHCConstants.h
@@ -0,0 +1,18 @@
+//
+//  BHCConstants.h
+//  BlackHorseCarriersApp
+//
+//  Created by Robert Stevenson on 3/1/13.
+//  Copyright (c) 2013 Robert Stevenson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *JOB_APPLICATION_URL;
+extern NSString *IPAY_ADP_URL;
+extern NSString *BLACKHORSE_WEBSITE;
+extern NSString *CONTACT_US_URL;
+extern NSString *GET_TERMINALS_URL;
+
+@interface BHCConstants : NSObject
+@end
---
+++ BlackHorseCarriersApp/BHCConstants.m
@@ -0,0 +1,19 @@
+//
+//  BHCConstants.m
+//  BlackHorseCarriersApp
+//
+//  Created by Robert Stevenson on 3/1/13.
+//  Copyright (c) 2013 Robert Stevenson. All rights reserved.
+//
+
+#import "BHCConstants.h"
+
+const NSString *JOB_APPLICATION_URL = @"https://www.blackhorsecarriers.com/application.php";
+const NSString *IPAY_ADP_URL = @"https://ipay.adp.com";
+const NSString *BLACKHORSE_WEBSITE = @"http://www.blackhorsecarriers.com";
+const NSString *CONTACT_US_URL = @"http://www.blackhorsecarriers.com/submitcontactus.php";
+const NSString *GET_TERMINALS_URL = @"http://www.blackhorsecarriers.com/phpsqlsearch_genxml.php?lat=37&lng=-122&radius=10025&initial=1";
+
+@implementation BHCConstants
+@end
+
