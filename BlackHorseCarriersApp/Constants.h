dffbfa523a2fb0b17cdcb3424c156a88aee7da07
Bob Stevenson <rstevenson555@comcast.net>
2013-03-19 06:17:08 -0500

    now use BHCConstants

--- BlackHorseCarriersApp/Constants.h
+++ BlackHorseCarriersApp/Constants.h
@@ -9,11 +9,6 @@
 #ifndef BlackHorseCarriersApp_Constants_h
 #define BlackHorseCarriersApp_Constants_h
 
-#define JOB_APPLICATION_URL @"https://www.blackhorsecarriers.com/application.php"
-#define IPAY_ADP_URL @"https://ipay.adp.com"
-//static NSString* CONTACT_US_URL = @"http://www.blackhorsecarriers.com/submitcontactus_test.php"
-#define BLACKHORSE_WEBSITE @"http://www.blackhorsecarriers.com"
-#define CONTACT_US_URL @"http://www.blackhorsecarriers.com/submitcontactus.php"
-#define GET_TERMINALS_URL @"http://www.blackhorsecarriers.com/phpsqlsearch_genxml.php?lat=37&lng=-122&radius=10025&initial=1"
+#import "BHCConstants.h"
 
 #endif
